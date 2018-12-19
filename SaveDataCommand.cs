384253e4cf7fb9f374a25a51689e2bd912cf8fa1
Karonuk <losha.pisnyak42@gmail.com>
2018-12-20 01:03:09 +0200

    InitialCommit

---
+++ SaveDataCommand.cs
@@ -0,0 +1,55 @@
+﻿using Smod2;
+using Smod2.Commands;
+using System;
+using System.Collections.Generic;
+
+namespace ExamplePlugin
+{
+    class SaveDataCommand : ICommandHandler
+    {
+        private Plugin plugin;
+        public SaveDataCommand(Plugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public string GetCommandDescription()
+        {
+            // This prints when someone types HELP HELLO
+            return "Save game info";
+        }
+
+        public string GetUsage()
+        {
+            // This prints when someone types HELP HELLO
+            return "Write save";
+        }
+
+        public string[] OnCall(ICommandSender sender, string[] args)
+        {
+            List<string> comandRes = new List<string>();
+            comandRes.Add("Command called");
+
+            string fName = args[0].Equals("") ? "output.txt" : args[0];
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(fName)) {
+                //Player num
+                int plCount = plugin.Server.NumPlayers;
+                file.WriteLine("Player: " + plCount);
+                //Num round
+
+
+                //Plugins
+                file.WriteLine("List Of Pluggins:");
+                plugin.pluginManager.Plugins.ForEach(e => { file.WriteLine; });
+                //Time
+                int totalSeconds = plugin.Server.;
+                int hours = totalSeconds / 3600;
+                int min = totalSeconds / 60;
+                int sec = totalSeconds % 60;
+                string totalTime = hours + ":" + min + ":" + sec;
+                file.WriteLine("Uptime:" + totalTime);
+            }
+            return new string[] { "Game succesfull saved!"};
+        }
+    }
+}
---
+++ SaveDataPlagin.cs
@@ -0,0 +1,39 @@
+﻿using Smod.TestPlugin;
+using Smod2;
+using Smod2.Attributes;
+using Smod2.EventHandlers;
+using Smod2.Events;
+
+namespace ExamplePlugin
+{
+	[PluginDetails(
+		author = "Courtney",
+		name = "Test",
+		description = "Example plugin",
+		id = "courtney.example.plugin",
+		version = "3.0",
+		SmodMajor = 3,
+		SmodMinor = 0,
+		SmodRevision = 0
+		)]
+	class SaveDataPlugin : Plugin
+	{
+		public override void OnDisable()
+		{
+		}
+
+		public override void OnEnable()
+		{
+			this.Info("Example Plugin has loaded :)");
+			this.Info("Config value: " + this.GetConfigString("test"));
+		}
+
+		public override void Register()
+		{
+           
+			this.AddCommand("SaveData",new SaveDataCommand(this));
+			// Register config settings
+			this.AddConfig(new Smod2.Config.ConfigSetting("test", "yes", Smod2.Config.SettingType.STRING, true, "test"));
+		}
+	}
+}
